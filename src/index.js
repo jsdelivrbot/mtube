4b81b38b49714bc5eadfc17bb42f3c81d00065c6
田中健 <takeru.tt5672@gmail.com>
2018-02-19 15:36:34 +0900

    [feature]Add SideVar which show Title and Thubnail

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+// API_KEYを読み込む
 import ENV from '../config/env'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = ENV['API_KEY']
 
-YTSearch({key: API_KEY , term: 'スノボ'}, function(data){ console.log(data) 
-})
-
 //Create new Component. This component should
 //produce some html.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    // クラスベースのコンポーネントのみ、どこでもthis.stateが利用できる
+    this.state = {videos: []}
+
+    YTSearch({key: API_KEY , term: 'one ok rock'}, (videos) => {
+      this.setState({videos}) //{videos: videos}の時略せる
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 //Take this component's generated HTML and put
 //it on the page(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'))
+
