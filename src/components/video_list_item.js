b75e3add281d9fb5cebbfe924234718d3873add0
田中健 <takeru.tt5672@gmail.com>
2018-02-19 19:50:03 +0900

    [feature]Update SearchBar

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // propsの代わりに {video}とすると下と同義
   // const video = props.video
   const imageUrl = video.snippet.thumbnails.default.url
   const title    = video.snippet.title
 
   return(
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
@@ -16,7 +16,7 @@ const VideoListItem = ({video}) => {
           <div className="media-body">
             <div className="media-heading">
               {title}
-             </div>
+             // </div>
           </div>
       </div>
     </li>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-// API_KEYを読み込む
+// Load YOUTUBE API KEY 
 import ENV from '../config/env'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = ENV['API_KEY']
 
@@ -16,18 +17,31 @@ class App extends Component {
   constructor(props){
     super(props)
     // クラスベースのコンポーネントのみ、どこでもthis.stateが利用できる
-    this.state = {videos: []}
-
-    YTSearch({key: API_KEY , term: 'one ok rock'}, (videos) => {
-      this.setState({videos}) //{videos: videos}の時略せる
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+    this.videoSearch('snowboard')
+  }
+  
+  videoSearch(term){
+    YTSearch({key: API_KEY , term: term}, (videos) => {
+    //this.setState({videos}) //{videos: videos}の時略せる
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
   render(){
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={(term) => {this.videoSearch(term)}}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     )
   }
